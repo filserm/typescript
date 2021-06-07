5279108713f839707bdc34f1de83913e60971a56
Michael Filser <filser.michael@gmail.com>
2021-06-07 14:41:13 +0200

    types

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-function combine(input1, input2) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
-    }
-    else {
-        result = input1.toString() + '&' + input2.toString();
-    }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(30, 6);
-console.log(combinedAges);
-var combinedNames = combine('Mike', 'Claudi');
-console.log(combinedNames);
+function printResult(num) {
+    console.log('The result is: ', num);
+}
+printResult(add(3, 5));
--- combinedtype.ts
+++ combinedtype.ts
@@ -1,15 +1,27 @@
-function combine(input1: number | string, input2: number | string) {
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text'
+
+function combine(
+    input1: Combinable, 
+    input2: Combinable,
+    resultConversion: ConversionDescriptor,
+) {
     let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     } else {
-        result = input1.toString() + '&' + input2.toString();
+        console.log('inp1:',input1, 'inp2:', input2)
+        result = input1.toString() + input2.toString();
     }    
-    return result
+    return result;
+    
 }
 
-const combinedAges = combine(30, 6)
+var combinedAges = combine(44, 36, 'as-number')
 console.log(combinedAges)
 
-const combinedNames = combine('Mike', 'Claudi')
+combinedAges = combine('44', '36', 'as-number')
+console.log(combinedAges)
+
+const combinedNames = combine('Mike', 'Claudi', 'as-text')
 console.log(combinedNames)
